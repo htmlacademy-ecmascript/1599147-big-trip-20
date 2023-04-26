e7701b34ac12d0662782ebf2c6b9a80581370d46
dimpulsha <dimpulsha@gmail.com>
2023-04-26 23:16:33 +0300

    попытка выделить отдельные элементы сортировки в компонент

--- src/templates/sort-item-template.js
+++ src/templates/sort-item-template.js
@@ -1,7 +1,7 @@
-const createSortItemTemplate = () => (`
+const createSortItemTemplate = (itemName) => (`
   <div class="trip-sort__item  trip-sort__item--day">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
-      <label class="trip-sort__btn" for="sort-day">Day</label>
+      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+      <label class="trip-sort__btn" for="sort-day">${itemName}</label>
     </div>
 `);
 
